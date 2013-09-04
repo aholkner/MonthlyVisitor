1f2188d28834ff52ced4d642a64e2785532a7523
Alex Holkner <alex@spryfox.com>
2013-09-04 18:37:42 +1000

    Import/export tilesets for whitespace change

--- res/Tileset-Interior.tsx
+++ res/Tileset-Interior.tsx
@@ -1,166 +1,166 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Interior" tilewidth="32" tileheight="32">
-  <image source="Tileset-Interior.png" width="512" height="512"/>
-  <terraintypes>
-   <terrain name="Back Wall" tile="-1"/>
-  </terraintypes>
-  <tile id="39">
-   <properties>
-    <property name="Fence" value="R"/>
-   </properties>
-  </tile>
-  <tile id="40">
-   <properties>
-    <property name="Fence" value="LR"/>
-   </properties>
-  </tile>
-  <tile id="41">
-   <properties>
-    <property name="Fence" value="L"/>
-   </properties>
-  </tile>
-  <tile id="42">
-   <properties>
-    <property name="StrongFence" value="R"/>
-   </properties>
-  </tile>
-  <tile id="43">
-   <properties>
-    <property name="StrongFence" value="LR"/>
-   </properties>
-  </tile>
-  <tile id="44">
-   <properties>
-    <property name="StrongFence" value="L"/>
-   </properties>
-  </tile>
-  <tile id="55">
-   <properties>
-    <property name="Fence" value="U"/>
-   </properties>
-  </tile>
-  <tile id="56">
-   <properties>
-    <property name="Fence" value="UD"/>
-   </properties>
-  </tile>
-  <tile id="57">
-   <properties>
-    <property name="Fence" value="D"/>
-   </properties>
-  </tile>
-  <tile id="58">
-   <properties>
-    <property name="StrongFence" value="U"/>
-   </properties>
-  </tile>
-  <tile id="59">
-   <properties>
-    <property name="StrongFence" value="UD"/>
-   </properties>
-  </tile>
-  <tile id="60">
-   <properties>
-    <property name="StrongFence" value="D"/>
-   </properties>
-  </tile>
-  <tile id="71">
-   <properties>
-    <property name="Fence" value="DR"/>
-   </properties>
-  </tile>
-  <tile id="72">
-   <properties>
-    <property name="Fence" value="DLR"/>
-   </properties>
-  </tile>
-  <tile id="73">
-   <properties>
-    <property name="Fence" value="DL"/>
-   </properties>
-  </tile>
-  <tile id="74">
-   <properties>
-    <property name="StrongFence" value="DR"/>
-   </properties>
-  </tile>
-  <tile id="75">
-   <properties>
-    <property name="StrongFence" value="DLR"/>
-   </properties>
-  </tile>
-  <tile id="76">
-   <properties>
-    <property name="StrongFence" value="DL"/>
-   </properties>
-  </tile>
-  <tile id="87">
-   <properties>
-    <property name="Fence" value="UDR"/>
-   </properties>
-  </tile>
-  <tile id="88">
-   <properties>
-    <property name="Fence" value="UDLR"/>
-   </properties>
-  </tile>
-  <tile id="89">
-   <properties>
-    <property name="Fence" value="UDL"/>
-   </properties>
-  </tile>
-  <tile id="90">
-   <properties>
-    <property name="StrongFence" value="UDR"/>
-   </properties>
-  </tile>
-  <tile id="91">
-   <properties>
-    <property name="StrongFence" value="UDLR"/>
-   </properties>
-  </tile>
-  <tile id="92">
-   <properties>
-    <property name="StrongFence" value="UDL"/>
-   </properties>
-  </tile>
-  <tile id="103">
-   <properties>
-    <property name="Fence" value="UR"/>
-   </properties>
-  </tile>
-  <tile id="104">
-   <properties>
-    <property name="Fence" value="ULR"/>
-   </properties>
-  </tile>
-  <tile id="105">
-   <properties>
-    <property name="Fence" value="UL"/>
-   </properties>
-  </tile>
-  <tile id="106">
-   <properties>
-    <property name="StrongFence" value="UR"/>
-   </properties>
-  </tile>
-  <tile id="107">
-   <properties>
-    <property name="StrongFence" value="ULR"/>
-   </properties>
-  </tile>
-  <tile id="108">
-   <properties>
-    <property name="StrongFence" value="UL"/>
-   </properties>
-  </tile>
-  <tile id="128" terrain=",,,0"/>
-  <tile id="129" terrain=",,0,0"/>
-  <tile id="130" terrain=",,0,"/>
-  <tile id="144" terrain=",0,,0"/>
-  <tile id="145" terrain="0,0,0,0"/>
-  <tile id="146" terrain="0,,0,"/>
-  <tile id="160" terrain=",0,,"/>
-  <tile id="161" terrain="0,0,,"/>
-  <tile id="162" terrain="0,,,"/>
- </tileset>
+<tileset name="Tileset-Interior" tilewidth="32" tileheight="32">
+ <image source="Tileset-Interior.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Back Wall" tile="-1"/>
+ </terraintypes>
+ <tile id="39">
+  <properties>
+   <property name="Fence" value="R"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="Fence" value="LR"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Fence" value="L"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="StrongFence" value="R"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="StrongFence" value="LR"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="StrongFence" value="L"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="Fence" value="U"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="Fence" value="UD"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="Fence" value="D"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="StrongFence" value="U"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="StrongFence" value="UD"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="StrongFence" value="D"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="Fence" value="DR"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="Fence" value="DLR"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="Fence" value="DL"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="StrongFence" value="DR"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="StrongFence" value="DLR"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="StrongFence" value="DL"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="Fence" value="UDR"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="Fence" value="UDLR"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="Fence" value="UDL"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="StrongFence" value="UDR"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="StrongFence" value="UDLR"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="StrongFence" value="UDL"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="Fence" value="UR"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="Fence" value="ULR"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="Fence" value="UL"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="StrongFence" value="UR"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="StrongFence" value="ULR"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="StrongFence" value="UL"/>
+  </properties>
+ </tile>
+ <tile id="128" terrain=",,,0"/>
+ <tile id="129" terrain=",,0,0"/>
+ <tile id="130" terrain=",,0,"/>
+ <tile id="144" terrain=",0,,0"/>
+ <tile id="145" terrain="0,0,0,0"/>
+ <tile id="146" terrain="0,,0,"/>
+ <tile id="160" terrain=",0,,"/>
+ <tile id="161" terrain="0,0,,"/>
+ <tile id="162" terrain="0,,,"/>
+</tileset>
--- res/Tileset-ObjectSpawns.tsx
+++ res/Tileset-ObjectSpawns.tsx
@@ -1,232 +1,232 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tileset-ObjectSpawns" tilewidth="32" tileheight="32">
-  <image source="Tileset-ObjectSpawns.png" width="256" height="256"/>
-  <tile id="0">
-   <properties>
-    <property name="Class" value="Tree"/>
-   </properties>
-  </tile>
-  <tile id="2">
-   <properties>
-    <property name="Anim" value="Item-Wood.png"/>
-    <property name="Class" value="Wood"/>
-   </properties>
-  </tile>
-  <tile id="3">
-   <properties>
-    <property name="Class" value="Rock"/>
-   </properties>
-  </tile>
-  <tile id="4">
-   <properties>
-    <property name="Class" value="Bone"/>
-   </properties>
-  </tile>
-  <tile id="5">
-   <properties>
-    <property name="Class" value="RawMeat"/>
-   </properties>
-  </tile>
-  <tile id="6">
-   <properties>
-    <property name="Class" value="Vegetable"/>
-   </properties>
-  </tile>
-  <tile id="7">
-   <properties>
-    <property name="Class" value="Pick"/>
-   </properties>
-  </tile>
-  <tile id="8">
-   <properties>
-    <property name="Class" value="Fire"/>
-   </properties>
-  </tile>
-  <tile id="9">
-   <properties>
-    <property name="Class" value="Fence"/>
-   </properties>
-  </tile>
-  <tile id="10">
-   <properties>
-    <property name="Class" value="StrongFence"/>
-   </properties>
-  </tile>
-  <tile id="11">
-   <properties>
-    <property name="Class" value="Grass"/>
-   </properties>
-  </tile>
-  <tile id="12">
-   <properties>
-    <property name="Class" value="Bread"/>
-   </properties>
-  </tile>
-  <tile id="13">
-   <properties>
-    <property name="Class" value="CookedMeat"/>
-   </properties>
-  </tile>
-  <tile id="14">
-   <properties>
-    <property name="Class" value="Stick"/>
-   </properties>
-  </tile>
-  <tile id="16">
-   <properties>
-    <property name="Class" value="Chicken"/>
-   </properties>
-  </tile>
-  <tile id="17">
-   <properties>
-    <property name="Class" value="Rabbit"/>
-   </properties>
-  </tile>
-  <tile id="18">
-   <properties>
-    <property name="Class" value="Axe"/>
-   </properties>
-  </tile>
-  <tile id="19">
-   <properties>
-    <property name="Class" value="Coal"/>
-   </properties>
-  </tile>
-  <tile id="20">
-   <properties>
-    <property name="Anim" value="TreeStump"/>
-   </properties>
-  </tile>
-  <tile id="22">
-   <properties>
-    <property name="Class" value="Steel"/>
-   </properties>
-  </tile>
-  <tile id="23">
-   <properties>
-    <property name="Class" value="Iron"/>
-   </properties>
-  </tile>
-  <tile id="26">
-   <properties>
-    <property name="Anim" value="BoneSkull"/>
-   </properties>
-  </tile>
-  <tile id="27">
-   <properties>
-    <property name="Anim" value="BoneLegs"/>
-   </properties>
-  </tile>
-  <tile id="28">
-   <properties>
-    <property name="Anim" value="BoneRibs"/>
-   </properties>
-  </tile>
-  <tile id="32">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="33">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="34">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="35">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="36">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="37">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="38">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="39">
-   <properties>
-    <property name="Blood" value=""/>
-   </properties>
-  </tile>
-  <tile id="40">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="41">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="42">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="43">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="44">
-   <properties>
-    <property name="BloodDribble" value=""/>
-   </properties>
-  </tile>
-  <tile id="56">
-   <properties>
-    <property name="FactoryClass" value="RawMeat"/>
-    <property name="Owner" value="Butcher"/>
-   </properties>
-  </tile>
-  <tile id="57">
-   <properties>
-    <property name="FactoryClass" value="Bread"/>
-    <property name="Owner" value="Baker"/>
-   </properties>
-  </tile>
-  <tile id="58">
-   <properties>
-    <property name="FactoryClass" value="CookedMeat"/>
-    <property name="Owner" value="Butcher"/>
-   </properties>
-  </tile>
-  <tile id="59">
-   <properties>
-    <property name="FactoryClass" value="Steel"/>
-    <property name="Owner" value="Blacksmith"/>
-   </properties>
-  </tile>
-  <tile id="60">
-   <properties>
-    <property name="FactoryClass" value="Iron"/>
-    <property name="Owner" value="Blacksmith"/>
-   </properties>
-  </tile>
-  <tile id="61">
-   <properties>
-    <property name="FactoryClass" value="Stick"/>
-    <property name="Owner" value="Carpenter"/>
-   </properties>
-  </tile>
-  <tile id="62">
-   <properties>
-    <property name="FactoryClass" value="Vegetable"/>
-    <property name="Owner" value="Farmer"/>
-   </properties>
-  </tile>
+ <image source="Tileset-ObjectSpawns.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="Class" value="Tree"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Anim" value="Item-Wood.png"/>
+   <property name="Class" value="Wood"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Class" value="Rock"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Class" value="Bone"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Class" value="RawMeat"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Class" value="Vegetable"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Class" value="Pick"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Class" value="Fire"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Class" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Class" value="StrongFence"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Class" value="Grass"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Class" value="Bread"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="Class" value="CookedMeat"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Class" value="Stick"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Class" value="Chicken"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Class" value="Rabbit"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Class" value="Axe"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Class" value="Coal"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Anim" value="TreeStump"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Class" value="Steel"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Class" value="Iron"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Anim" value="BoneSkull"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Anim" value="BoneLegs"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Anim" value="BoneRibs"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Blood" value=""/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="BloodDribble" value=""/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="FactoryClass" value="RawMeat"/>
+   <property name="Owner" value="Butcher"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="FactoryClass" value="Bread"/>
+   <property name="Owner" value="Baker"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="FactoryClass" value="CookedMeat"/>
+   <property name="Owner" value="Butcher"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="FactoryClass" value="Steel"/>
+   <property name="Owner" value="Blacksmith"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="FactoryClass" value="Iron"/>
+   <property name="Owner" value="Blacksmith"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="FactoryClass" value="Stick"/>
+   <property name="Owner" value="Carpenter"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="FactoryClass" value="Vegetable"/>
+   <property name="Owner" value="Farmer"/>
+  </properties>
+ </tile>
 </tileset>
